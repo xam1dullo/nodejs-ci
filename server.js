895dff4fd20100659404f0c758602420cb3673d9
xamidullo <khkhamidullo@gmail.com>
2025-01-10 16:08:02 +0500

    update server.js

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
-const PORT =process.env.PORT || 4040
-
+const PORT = process.env.PORT || 4040;
 
 const app = express();
 
-app.use("/greet", (req, res) => {
+app.use('/greet', (req, res) => {
   res.send('SALOMLAR!');
 });
 
-app.use("/app", (req, res) => {
+app.use('/users', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      name: 'ali',
+      age: 22,
+      isMerred: false,
+    },
+  ]);
+});
+
+app.use('/app', (req, res) => {
   res.send('APPLOCATION!');
 });
 
 app.use((req, res) => {
   res.send('ASSALOMU ALAYKUM!.');
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
